6e1e12c39062e938e0757936900c53fc5792ddc1
Bhupesh <Bhupesh1755@gmail.com>
2023-05-22 23:59:02 +0530

    Modified variable,c

--- variable.c
+++ variable.c
@@ -3,17 +3,22 @@
 int b=34;
 
 int func1(int b1) {
-    printf("The value of b inside func1 is %d\n", b);
+    static int myvar= 98;
+    printf("The value of myvar is %d\n", myvar);
+    myvar ++;
+    // printf("The value of b inside func1 is %d\n", b);
     // printf("The address of b inside func1 is %d\n", &b);
-    return b1*10;
+    return b1+myvar ;
 }
 
 int main() {
     int b =344;
     // printf("The address of b inside main is %d\n", &b);
     int val= func1(b);
+    val= func1(b);
+    val= func1(b);
     int *ptra= &val;
-    printf("The value of func1 is %d\n", val);
+    // printf("The value of func1 is %d\n", val);
 
     return 0;
 }
