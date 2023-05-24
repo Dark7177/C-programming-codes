8fd269c43c124d222b9bad9be4219f03ce6b8226
Bhupesh <Bhupesh1755@gmail.com>
2023-05-24 23:45:50 +0530

    Added the ques

--- travel_agency_exer.c
+++ travel_agency_exer.c
@@ -1,3 +1,10 @@
+// You manage a travel agency and want n driver to input thier following detail:
+// Name
+// Driving license number  
+// Route 
+// Kms
+// The program should be able to take n as input (or we can take n =3 for simplicity) and your driver will start inputing their detail one by one. the program should print details of drivers in beautiful fashon .For this task you have to use structure only.These four input :name, driving license number route and km information shall be sruct member . after creating strruct you have to create its array of size "n".
+
 #include<stdio.h>
 
 int main() {
