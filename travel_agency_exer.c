4721341c675cf6dcdda3b25644879343bcca8ba3
Bhupesh <Bhupesh5676l@gmail.com>
2023-05-25 20:15:27 +0530

    Successfully compiled code

--- travel_agency_exer.c
+++ travel_agency_exer.c
@@ -18,19 +18,62 @@ struct driver
 
 int main()
 {
-    printf("Enter your name as per our records\n");
-    scanf("%s", &driver_1.Name);
-    printf("Enter your license no as per record\n");
-    scanf("%s", &driver_1.License_no);
-    printf("Enter your Route detail\n");
-    scanf("%s", &driver_1.Route);
-    printf("Enter your drivem kms\n");
-    scanf("%f", &driver_1.kms);
+    for (int i = 1; i < 4; i++)
+    {
+        switch (i=1,2,3)
+        {
+        case 1:
+            printf("Enter your name as per our records\n");
+            scanf("%s", &driver_1.Name);
+            printf("Enter your license no as per record\n");
+            scanf("%s", &driver_1.License_no);
+            printf("Enter your Route detail\n");
+            scanf("%s", &driver_1.Route);
+            printf("Enter your drivem kms\n");
+            scanf("%f", &driver_1.kms);
 
+        case 2:
+            printf("Enter your name as per our records\n");
+            scanf("%s", &driver_2.Name);
+            printf("Enter your license no as per record\n");
+            scanf("%s", &driver_2.License_no);
+            printf("Enter your Route detail\n");
+            scanf("%s", &driver_2.Route);
+            printf("Enter your drivem kms\n");
+            scanf("%f", &driver_2.kms);
+
+        case 3:
+            printf("Enter your name as per our records\n");
+            scanf("%s", &driver_3.Name);
+            printf("Enter your license no as per record\n");
+            scanf("%s", &driver_3.License_no);
+            printf("Enter your Route detail\n");
+            scanf("%s", &driver_3.Route);
+            printf("Enter your drivem kms\n");
+            scanf("%f", &driver_3.kms);
+
+            goto print;
+        default:
+            break;
+        }
+    }
+
+print:
+
+    printf("Details saved successfully. Kindly check if any errors\n");
     printf("Your entered name is %s\n", driver_1.Name);
     printf("Your licenso no is %s\n", driver_1.License_no);
     printf("Your entered name is %s\n", driver_1.Route);
     printf("Your entered name is %f\n", driver_1.kms);
 
+    printf("Your entered name is %s\n", driver_2.Name);
+    printf("Your licenso no is %s\n", driver_2.License_no);
+    printf("Your entered name is %s\n", driver_2.Route);
+    printf("Your entered name is %f\n", driver_2.kms);
+
+    printf("Your entered name is %s\n", driver_3.Name);
+    printf("Your licenso no is %s\n", driver_3.License_no);
+    printf("Your entered name is %s\n", driver_3.Route);
+    printf("Your entered name is %f\n", driver_3.kms);
     return 0;
 }
