c64d93f73093a6f8fd38643ef8862213a88ef36d
Bhupesh <Bhupesh5676l@gmail.com>
2023-05-27 00:45:35 +0530

    Added the problem

--- Employee_manager_exer.c
+++ Employee_manager_exer.c
@@ -1,3 +1,10 @@
+// ABC pvt ltd manages employee record of other companies
+// Employees Id can be of any length and it can contain any characters
+// For 3 employees ,we have to take length of employee id as an input in legnth integer variable .
+// Then we have to take employee id as input and display it on screen. 
+// Store the employeee if in a character array which is allocated as dynamically
+// We have to create only one charcter array dynamically
+
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
