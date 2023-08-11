40af30305f7bfc9dd3672861055125005d1c3051
Dark <Bhupesh1755@gmail.com>
2023-08-11 23:53:23 +0530

    Added code

--- storage_classes.c
+++ storage_classes.c
@@ -2,19 +2,76 @@
 // Declaration - Telling the compiler about the variable(No Space reserved)
 // Defination - Declaration + space Reservation
 
-#include <stdio.h>
-#include <string.h>
+// Auto Storage Class Code
 
+// #include <stdio.h>
+// #include <string.h>
+
+// int myfunc(int a, int b)
+// {
+//     auto int sum;
+//     sum = a + b;
+//     return sum;
+// }
+
+// int main()
+// {
+//     int sum = myfunc(3, 5);
+//     printf("The Sum is %d\n", sum);
+//     return 0;
+// }
+
+// External Variable class
+
+// #include<stdio.h>
+// int sum =453;
+
+// int myfunc(int a , int b){
+//     extern int sum;
+//     // sum = a+b ;
+//     return sum;
+// }
+
+// int main () {
+//     int sum = myfunc(10 , 5);
+//     printf("The sum is %d\n", sum);
+//     return 0;
+// }
+
+// Static Variable class
+
+// #include<stdio.h>
+
+// int myfunc(int a , int b){
+//     static int myVar;
+//     printf("The value of myVar is %d\n", myVar);
+//     myVar++;
+//     return myVar;
+// }
+
+// int main () {
+//     int myVar = myfunc(10,5);
+//     myVar = myfunc(10,5);
+//     myVar = myfunc(10,5);
+//     myVar = myfunc(10,5);
+//     return 0;
+// }
+
+// Register Storage Class
+#include <stdio.h>
 int myfunc(int a, int b)
 {
-    int sum;
-    sum = a + b;
-    return sum;
+    static int myVar = 0;
+    myVar++;
+    printf("The value of myVar is %d\n", myVar);
+    return myVar;
 }
 
 int main()
 {
-    int sum = myfunc(3, 5);
-    printf("The Sum is %d\n", sum);
-     return 0;
+    register int myVar = myfunc(10, 7);
+    myVar = myfunc(10, 7);
+    myVar = myfunc(10, 7);
+    myVar = myfunc(10, 7);
+    return 0;
 }
